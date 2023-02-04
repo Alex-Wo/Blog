3e0c0057af21e8ce0d52057b075b281c86a01ed5
Alex-Wo <blackdevops13@gmail.com>
2023-02-04 20:43:20 +0300

    Добавлена система комментариев и модель управления ими в админке

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,13 +1,16 @@
 # Здесь мы регистрируем модели для добавления их в систему администрирования Django.
 
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 # Настройка отображения модели в админке (фильтр постов по времени, дате и статусу)
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    """
+        Модель для управления постами в админке.
+    """
     list_display = ('title', 'slug', 'author', 'publish', 'status')  # Это наши отображаемые в списке статей поля
     list_filter = ('status', 'created', 'publish', 'author')  # Фильтр, сортирующий статьи по полям указанным выше
     search_fields = ('title', 'body')  # Строка поиска
@@ -16,3 +19,12 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'publish'  # Атрибут, добавляющий ссылки для навигации по датам
     ordering = ('status', 'publish')  # Настройка сортировки, которая выше
 
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+        Модель для управления комментариями в админке.
+    """
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -58,3 +58,26 @@ class Post(models.Model):
                              self.publish.month,
                              self.publish.day,
                              self.slug])
+
+
+class Comment(models.Model):
+    """
+        Модель комментария. Она содержит ForeignKey для привязки к определённой статье. Это отношение определено как
+        "один ко многим", т.к. одна статья может иметь множество комментариев, но каждый комментарий (!) может быть
+        оставлен только для одной статьи.
+
+        Булево значение в поле 'active' добавлено для возможности скрыть некоторые комментарии, например в целях цензуры.
+    """
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)
